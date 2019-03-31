c1a62d85348f0fcf145ea1430c37bf5247d904f9
FernandaPalacios <ferpalacios.a@gmail.com>
2019-03-31 15:29:06 -0400

    sample wav files

--- Milestone2/wav_to_bin.py
+++ Milestone2/wav_to_bin.py
@@ -11,57 +11,62 @@
 # Print to stdout the number of seconds that were analyzed 
 # and the number of values averaged per second
 
-
 import os
 import sys
+import wave
 
 FILENAME = sys.argv[1] 
-MAX_VALUES_PER_SECOND = 9 - 1 # estimated this value by looking at 1s long .wav file
+wf  = wave.open('wav_files/' + FILENAME + '.wav', "rb")
+SAMPLE_RATE = int(wf.getframerate())
 
-# os.system("od -A d -j 44 -t d2 "  + 'wav_files/' + FILENAME + '.wav > ' +  'txt_files/' + FILENAME + '.txt' )
+if(SAMPLE_RATE == 44100):
+	# this sample rate requires adjust
+	SAMPLE_RATE = 44100*2
 
+MAX_VALUES_PER_SECOND = SAMPLE_RATE - 1 # estimated this value by looking at 1s long .wav file
+
+os.system("od -A d -j 44 -t d2 "  + 'wav_files/' + FILENAME + '.wav > ' +  'txt_files/' + FILENAME + '.txt' )
 f_read = open('txt_files/' + FILENAME + ".txt", "r")
 f_write = open('txt_files/' + FILENAME + "_bin.txt", "a")
 
 
-curr_value_num = 1
+num_seconds = 0
+curr_value_num = 0
 curr_sum = 0
 curr_avg = 0
 for line in f_read:
 	line = line.split()
-	# loop over values in each line
-	if(len(line) == 9):
-		# skip 0th element
-		for i in range (1, 9):
+	if(len(line) > 1):
+		for i in range (1, len(line)):
+			try:
+				curr_value = abs(int(line[i]))
+				
 
-			curr_value = abs(int(line[i]))
-			curr_sum += curr_value
-			print(curr_value)
-
-
-			if(curr_value_num == MAX_VALUES_PER_SECOND):
-				print("reached max")
-				curr_avg = round(curr_sum/MAX_VALUES_PER_SECOND)
-				curr_value_num = 1
-				curr_sum = 0
-				print("avg: " + str(curr_avg))
-
+			except:
+				#go to next line
+				break
 			else:
-				curr_value_num+=1
 
+				curr_value_num+=1
+				curr_sum += curr_value
+
+				if(curr_value_num == MAX_VALUES_PER_SECOND):
+					curr_avg = round(curr_sum/MAX_VALUES_PER_SECOND)
+					print("reached max", " avg:", curr_avg)
+					curr_value_num = 0
+					curr_sum = 0
+					num_seconds+=1
 
 
 # check if we still need to write average for last second
 if(curr_value_num != 0):
-	print("would write one more avg")
-	print("sum:", curr_sum)
-	curr_avg = round(curr_sum/MAX_VALUES_PER_SECOND)
-	print(curr_avg)
+	curr_avg = round(curr_sum/curr_value_num)
+	print("last avg: ", curr_avg)
+	num_seconds+=1
+
+
+print("number of seconds: ", num_seconds)
+print("number of values per second: ", MAX_VALUES_PER_SECOND)
 
 f_read.close()
 f_write.close()
-
-
-
-
-
